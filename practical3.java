b577b1210212731d5714c05a4d243e47dc580c47
ragulraj1U24CA084 <rp684948@gmail.com>
2026-07-19 18:34:59 +0530

    Update practical3.java

--- practical3.java
+++ practical3.java
@@ -1,62 +1,34 @@
-package practical3;
-
-
-
 import java.util.Scanner;
-
-
-
 import java.util.ArrayList;
-
-public class practical3 {
-
-	public static void main(String[] args)
-
-    	{
-
-        	Scanner scanner = new Scanner(System.in);
-
-            	System.out.println("Enter a line of text:");
-
-                	String inputLine = scanner.nextLine();
-
-                    	String[] words = inputLine.split("");
-
-                        	ArrayList<String>wordsStartingWithB = new ArrayList<>();
-
-                            	for (String word : words)
-
-                                	{
-
-                                    	if (word.toLowerCase().startsWith("b"))
-
-
-
-                                        	{
-
-                                            	wordsStartingWithB.add(word);
-
-                                                	}
-
-                                                    	}
-
-                                                        	ArrayList<String>wordsEndingWithED = new ArrayList<>();
-
-                                                            	for (String word : words)
-
-                                                                	{
-
-                                                                    	if (word.toUpperCase().endsWith("ED"))
-
-
-
-                                                                        	{
-
-                                                                            	wordsEndingWithED.add(word);
-
-                                                                                	}
-
-                                                                                    	}
+class practical3
+{
+	public static void main(String[]args)
+	{
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter a line of text:");
+	String inputLine=scanner.nextLine();
+	String[] words=inputLine.split(" ");
+	ArrayList<String> wordsStartingWithB=new ArrayList<>();
+	for(String word:words)
+	{
+	   if(word.toLowerCase().startsWith("b"))
+	   {
+		   wordsStartingWithB.add(word);
+		   
+	   }
+	}
+	ArrayList<String> wordsEndingWithED=new ArrayList<>();
+	for(String word:words) 
+	{
+		if(word.toUpperCase().endsWith("ED"))
+		{
+			wordsEndingWithED.add(word);
+		}
+	}
+	System.out.println("Words starting with 'b': "+wordsStartingWithB);
+	System.out.println("Words ending with 'ED': "+wordsEndingWithED);
+		}
+	}
 
                                                                                         	System.out.println("Words starting with 'b':"+ wordsStartingWithB);
 
